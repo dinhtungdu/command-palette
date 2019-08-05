9a3403ae856d8731c2dcb4a7669b4ec7c51fbb3a
Tung Du <dinhtungdu@gmail.com>
2019-08-05 20:02:54 +0700

    add icon to search box

--- templates/command-box.php
+++ templates/command-box.php
@@ -1,16 +1,17 @@
 <div id="scp-command-wrapper">
   <div id="scp-command-dialog">
-    <header>
-      <span class="title">
-        <?php echo esc_html__( 'Command Palette', 'snappy-command-palette' ); ?>
-      </span>
-      <span class="divider"></span>
-      <span class="close">
-        <span class="dashicons dashicons-no-alt"></span>
-      </span>
-    </header>
-    <div id="scp-command-search">
-      <input type="text" placeholder="<?php echo esc_attr__( 'Type to find anything', 'snappy-command-palette' ); ?>">
-    </div>
+	<header>
+	  <span class="title">
+		<?php echo esc_html__( 'Command Palette', 'snappy-command-palette' ); ?>
+	  </span>
+	  <span class="divider"></span>
+	  <span class="close">
+		<span class="dashicons dashicons-no-alt"></span>
+	  </span>
+	</header>
+	<div id="scp-command-search">
+	  <span class="dashicons dashicons-search"></span>
+	  <input type="text" placeholder="<?php echo esc_attr__( 'Type to find anything', 'snappy-command-palette' ); ?>">
+	</div>
   </div>
 </div>
