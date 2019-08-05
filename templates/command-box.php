f0b535fff146812bb826c9386c55437c2b9416a1
Tung Du <dinhtungdu@gmail.com>
2019-08-05 19:45:39 +0700

    command dialog template

--- templates/command-box.php
+++ templates/command-box.php
@@ -1,2 +1,15 @@
-<div id="scp-command-box">
+<div id="scp-command-wrapper">
+  <div id="scp-command-dialog">
+    <header>
+      <span class="title">
+        <?php echo esc_html__( 'Command Palette', 'snappy-command-palette' ); ?>
+      </span>
+      <span class="close">
+        <span class="dashicons dashicons-no-alt"></span>
+      </span>
+    </header>
+    <div id="scp-command-search">
+      <input type="text" placeholder="<?php echo esc_attr__( 'Type to find anything', 'snappy-command-palette' ); ?>">
+    </div>
+  </div>
 </div>
