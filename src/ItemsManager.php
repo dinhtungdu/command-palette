37a18d3398e7855e8e2763f6b0620c4fd4651b0e
Tung Du <dinhtungdu@gmail.com>
2019-08-08 08:56:17 +0700

    Clean up

--- src/ItemsManager.php
+++ src/ItemsManager.php
@@ -2,39 +2,4 @@
 namespace CommandPalette;
 
 class ItemsManager {
-	private $items;
-
-	public function getAll() {
-		return $this->items;
-	}
-
-	public function get( $key ) {
-		if ( ! in_array( $key, $this->items ) ) {
-			return null;
-		}
-
-		return $this->items[ $key ];
-	}
-
-	public function add( $data ) {
-		$default = [
-			'type'     => 'url',
-			'key'      => '',
-			'url'      => '',
-			'script'   => '',
-			'category' => __( 'General', 'command-palette' ),
-		];
-
-		$data = wp_parse_args( $data, $default );
-
-		if ( ! $data['key'] && ! $data['url'] ) {
-			return;
-		}
-
-		$this->items[ $data['key'] ] = [
-			'url'      => $data['url'],
-			'category' => $data['category'],
-		];
-	}
-
 }
