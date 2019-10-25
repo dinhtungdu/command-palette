98cacdfef3e80f4283819d70d4bda1dda970ca6f
Tung Du <dinhtungdu@gmail.com>
2019-10-25 07:24:59 +0700

    add doc comment

--- src/AssetManager.php
+++ src/AssetManager.php
@@ -1,12 +1,21 @@
 <?php
+/**
+ * Manage assets used for the plugin.
+ *
+ * @package CommandPalette
+ */
+
 namespace CommandPalette;
 
+/**
+ * Class: AssetManager
+ */
 class AssetManager {
 	public function hooks() {
-		add_action( 'admin_enqueue_scripts', [ $this, 'register_scripts' ] );
+		add_action( 'admin_enqueue_scripts', [ $this, 'registerScripts' ] );
 	}
 
-	public function register_scripts() {
+	public function registerScripts() {
 		wp_enqueue_script( 'command-palette-main', SCP_URL . 'assets/js/main.js', [ 'jquery' ], SCP_VER, true );
 		wp_enqueue_style( 'command-palette-main', SCP_URL . 'assets/css/main.css', [], SCP_VER );
 
